2196186b8e2282705b5346eec8d565f56915ad6f
josmorales1 <josgerardomorales1@aol.com>
2019-10-08 15:02:04 -0500

    I change the layout. Did a lot of work on the styling part. Javascript was finished just needed to chagne a couple of line of codes. I tried to have a space theme something different

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,105 +1,80 @@
-$(document).ready(function(){
-    // Generate random number to guess
-    var Random=Math.floor(Math.random()*120+19)
+$(document).ready(function () {
+    var crystalOrange = Math.floor(Math.random() * 12 + 1);
+    var crystalPurple = Math.floor(Math.random() * 12 + 1);
+    var crystalRed = Math.floor(Math.random() * 12 + 1);
+    var compChoice = Math.floor(Math.random() * 102 + 19);
     
-    // Display random number
-    $('#scoreToMatch').text(Random);
-    
-    //Generate random number for each crystal
-    var num1= Math.floor(Math.random()*12+1)
-    var num2= Math.floor(Math.random()*12+1)
-    var num3= Math.floor(Math.random()*12+1)
-    var num4= Math.floor(Math.random()*12+1)
-    console.log("Red: " + num1, "Blue: " + num2, "Yellow: " + num3, "Green: " + num4);
-    
-    // Variables to keep track of wins, losses and total
-    var playerTotal= 0; 
-    var wins= 0;
-    var losses = 0;
-  
-    $('#winner1,#winner2').hide();
-    $('#lose1,#lose2').hide();  
-    $('#wins').text(wins);
-    $('#losses').text(losses);
-  
 
-  
-  // Display wins
-  function winner(){
-  //alert("Congrats! You won!");
-    wins++; 
-    $('#wins').text(wins);
-    $('#winner1,#winner2').show();
-    reset();
-  }
-  
-  // Display losses
-  function loser(){
-    //alert ("Sorry! You lose!");
-    losses++;
-    $('#losses').text(losses);
-    $('#lose1,#lose2').show();    
-    reset();
-  }
-  
-  // Clicking crystals
-    $('.red').on ('click', function(){
-      playerTotal = playerTotal + num1;
-      $('#totalScore').text(playerTotal); 
+    // the variables that will appear on the screen:
+    $("#matchNum").text("Match this number: " + compChoice);
+    var result = 0; // this will show the total # that the user has accumulated
+    $("#userNum").text(result);
+    var wins = 0; // will go up if result === compChoice
+    $(".wins").text("Wins: " + wins)
+    var losses = 0; // will go up if they surpass target # (result > compChoice)
+    $(".losses").text("Losses: " + losses);
 
-  //Win & lose conditions
-    if (playerTotal == Random){
-        winner();
+
+    function winner() {
+        wins++;
+        $(".wins").text("Wins: " + wins);
     }
-    else if ( playerTotal > Random){
-        loser();
-        reset();
-    }   
-    })
+
+    function loser() {
+        losses++;
+        $(".losses").text("Losses: " + losses);
+    };
+
+    function surpassed() {
+        if (result > compChoice)
+        result = 0;
+        $("#userNum").text(result);
+        crystalOrange = Math.floor(Math.random() * 12 + 1);
+        crystalPurple = Math.floor(Math.random() * 12 + 1);
+        crystalRed = Math.floor(Math.random() * 12 + 1);
+        compChoice = Math.floor(Math.random() * 102 + 19);
+
+        $("#matchNum").text("Match this number: " + compChoice);
+    }
+
     
-// Image click functions
-    $('.blue').on ('click', function(){
-      playerTotal = playerTotal + num2;
-      $('#totalScore').text(playerTotal); 
-          if (playerTotal == Random){
-            winner();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          } 
-    })  
-    $('.yellow').on ('click', function(){
-      playerTotal = playerTotal + num3;
-      $('#totalScore').text(playerTotal);
-  
-            if (playerTotal == Random){
-            winner();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          } 
-    })  
-    $('.green').on ('click', function(){
-      playerTotal = playerTotal + num4;
-      $('#totalScore').text(playerTotal); 
+    $(".crystalOrange").on("click", function () {
+        result = result + crystalOrange;
         
-            if (playerTotal == Random){
+        $("#userNum").text(result);
+
+        if (result === compChoice) {
             winner();
-          }
-          else if ( playerTotal > Random){
+        } else if (result > compChoice) {
             loser();
-          }
-    }); 
-    
-// Reset game
-  function reset(){
-    Random=Math.floor(Math.random()*120+19);
-    $('#scoreToMatch').text(Random);
-    num1= Math.floor(Math.random()*12+1);
-    num2= Math.floor(Math.random()*12+1);
-    num3= Math.floor(Math.random()*12+1);
-    num4= Math.floor(Math.random()*12+1);
-    playerTotal= 0;
-    $('#totalScore').text(playerTotal);
-    } 
-  }); 
+            surpassed();
+        }
+    })
+
+    $(".crystalPurple").on("click", function () {
+        result = result + crystalPurple;
+        
+        $("#userNum").text(result);
+
+        if (result === compChoice) {
+            winner();
+        } else if (result > compChoice) {
+            loser();
+            surpassed();
+        }
+    })
+
+    $(".crystalRed").on("click", function () {
+        result = result + crystalRed;
+        
+        $("#userNum").text(result);
+
+        if (result === compChoice) {
+            winner();
+        } else if (result > compChoice) {
+            loser();
+            surpassed();
+        }
+    })
+
+});
